a45c1d3d402c41fc6143ae59510abe440512a45b
qsqnk <st087201@student.spbu.ru>
2022-11-04 04:46:34 +0300

    Refactor

--- src/main/java/application/Utils.kt
+++ src/main/java/application/Utils.kt
@@ -1,11 +1,10 @@
 package application
 
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-inline fun <reified T> T.getLogger() = LoggerFactory.getLogger(
+inline fun <reified T : Any> T.getLogger(): Logger = LoggerFactory.getLogger(
     if (T::class.isCompanion) T::class.java.enclosingClass else T::class.java
 )
-
-inline fun <reified T : Enum<T>> lowercaseNamesOf() = enumValues<T>().map { it.name.lowercase() }
 
 fun Enum<*>.lowercaseName() = name.lowercase()
